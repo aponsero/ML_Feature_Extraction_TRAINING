4126fe000e6b63114e9f16885975429925454f1b
aponsero <alise.ponsero@gmail.com>
2017-08-04 23:07:44 +0200

    Update and rename config.sh to scripts/config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,11 +1,9 @@
 export CWD=$PWD
-# where programs are
-export BIN_DIR="/rsgrps/bhurwitz/hurwitzlab/bin"
 # where the dataset to prepare is
-export DATASET="/rsgrps/bhurwitz/alise/my_data/Machine_learning/Machine2/training_set/Vir_trainingset/vir_trainingset.fa"
+export DATASET="YOUR/FILE"
 export TYPE="viral"
 #either "viral" or "bacterial"
-export RESULT_DIR="/rsgrps/bhurwitz/alise/temp/test_features"
+export RESULT_DIR="YOUR/DIR"
 #place to store the scripts
 export SCRIPT_DIR="$PWD/scripts"
 export WORKER_DIR="$SCRIPT_DIR/workers"
@@ -15,11 +13,11 @@ export MIN_SIZE=500
 export SPLIT_SIZE=100
 export NUM_FILES=2
 # kmer size
-export KM_SIZE=3
+export KM_SIZE=8
 # User informations
 export QUEUE="standard"
-export GROUP="bhurwitz"
-export MAIL_USER="aponsero@email.arizona.edu"
+export GROUP="yourgroup"
+export MAIL_USER="yourmail@email.arizona.edu"
 export MAIL_TYPE="bea"
 
 #
